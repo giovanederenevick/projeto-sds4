41ff81dc561875281207f9184e2c5ca833047d1a
Giovane Derenevick Filho <giovanedfilho@gmail.com>
2021-09-10 13:49:13 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,27 @@
+import { type } from 'os';
+import { Seller } from './seller';
+
+export type Sale = {
+    id: number;
+    visited: number;
+    deals: number;
+    amount: number;
+    date: string;
+    seller: Seller;
+}
+
+export type SalePage = {
+    content?: Sale[];
+    last: boolean;
+    totalPages: number;
+    totalElements: number;
+    first: boolean;
+    number: number;
+    numberOfElements?: number;
+    size?: number;
+    empty?: boolean;
+}
+
 export type SaleSum = {
     sellerName: string;
     sum: number;
